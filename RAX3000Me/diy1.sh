d465d3a5f38a6465a4d2bd1562592ff391e7ec7f
Gu888 <142123343+Gu888@users.noreply.github.com>
2025-03-12 16:33:41 +0800

    Update diy1.sh

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -12,18 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
-# 自定义软件源
-git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages package/openwrt-packages
-# Mosdns
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-#rm -rf feeds/packages/net/v2ray-geodata
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# Easytier
-git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git luci-app-easytier
-mv luci-app-easytier/easytier package/easytier
-mv luci-app-easytier/luci-app-easytier package/luci-app-easytier
-rm -rf luci-app-easytier
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#git clone https://github.com/messense/aliyundrive-webdav package/messense
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
